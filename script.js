d3de8a6a33598adeb2c8db7a9d210ee80819279a
Alex Rocha <rocha.amc@gmail.com>
2023-01-12 11:49:40 -0800

    Add paddle and keyboard controls

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
+const ballRadius = 10;
+const paddleHeight = 10;
+const paddleWidth = 75;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
-const ballRadius = 10;
 let ballColour = "#0095DD";
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
+
 
 function newRGBNumber() {
   return Math.floor(Math.random() * 255)
 }
+
 function changeColour() {
   const red = newRGBNumber()
   const green = newRGBNumber()
@@ -28,7 +35,6 @@ function drawBall() {
     ballColour = changeColour();
   }
 
-
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = ballColour;
@@ -36,11 +42,45 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   x += dx;
   y += dy;
+  if (rightPressed) {
+    paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+  } else if (leftPressed) {
+    paddleX = Math.max(paddleX - 7, 0);
+  }
+  drawPaddle();
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = false;
+  }
 }
 
 setInterval(draw, 10);
