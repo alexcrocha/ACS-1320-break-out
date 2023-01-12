439f8468693dd7d2790805e9b8b7644dc9f938f5
Alex Rocha <rocha.amc@gmail.com>
2023-01-12 11:26:39 -0800

    Bounce off the walls

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+const ballRadius = 10;
+
 
 function drawBall() {
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
+
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
