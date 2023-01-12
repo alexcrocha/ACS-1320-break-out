b233f67ea3983995c45103778956765007e15a4f
Alex Rocha <rocha.amc@gmail.com>
2023-01-12 11:38:40 -0800

    Change colour when ball hits the wall

--- script.js
+++ script.js
@@ -6,20 +6,32 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 const ballRadius = 10;
+let ballColour = "#0095DD";
 
+function newRGBNumber() {
+  return Math.floor(Math.random() * 255)
+}
+function changeColour() {
+  const red = newRGBNumber()
+  const green = newRGBNumber()
+  const blue = newRGBNumber()
+  return `rgb(${red},${green},${blue})`
+}
 
 function drawBall() {
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
+    ballColour = changeColour();
   }
   if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     dy = -dy;
+    ballColour = changeColour();
   }
 
 
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = ballColour;
   ctx.fill();
   ctx.closePath();
 }
