ce1934dfa3bf317c9b0ddf92c35bd6b6ac55e783
Alex Rocha <rocha.amc@gmail.com>
2023-01-15 17:23:03 -0800

    Add Mouse controls

--- script.js
+++ script.js
@@ -111,6 +111,14 @@ function draw() {
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function mouseMoveHandler(e) {
+  const relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > paddleWidth / 2 && relativeX < canvas.width - paddleWidth / 2) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+}
 
 function keyDownHandler(e) {
   if (e.key === "Right" || e.key === "ArrowRight" || e.key === "d") {
